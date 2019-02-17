a72fe35deafe9a125f323e244ad1d7fa15a186e8
Chris Strutton <chris@rodandfly.ca>
2019-02-17 12:29:07 -0500

    cleanup and add vue router and vuetify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vue from 'vue'
 import App from './App.vue'
 
+import Vuetify from 'vuetify'
+import VueRouter from 'vue-router'
+import 'vuetify/dist/vuetify.min.css'
 Vue.config.productionTip = false
+Vue.use(Vuetify, {
+  iconfont: 'mdi'
+})
+
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App)
