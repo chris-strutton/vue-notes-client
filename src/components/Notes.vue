dd758eddb5b33a1c39bcb59a45184c339f3fdd59
Chris Strutton <chris@rodandfly.ca>
2019-02-18 07:52:23 -0500

    added list display page

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,10 +1,29 @@
 <template>
-  <div></div>
+  <v-layout column>
+    <v-flex xs-8>
+      <panel title="Search for Notes">
+        <v-text-field label="Note Title"></v-text-field>
+      </panel>
+      <panel title="Notes">
+        <v-btn icon fab right to="create" slot="action" class="indigo darken-2">
+          <v-icon>add</v-icon>
+        </v-btn>
+        <div v-for="note in notes" :key="note.title">
+          {{ note.title }}
+          {{ note.text }}
+          {{ note.author }}
+        </div>
+      </panel>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
+import Panel from '@/components/Panel'
 export default {
-
+  components: {
+    Panel
+  }
 }
 </script>
 
--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,9 +1,23 @@
 <template>
-  <div></div>
+  <div class="white elevation-2">
+    <v-toolbar flat dense class="indigo" dark>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <slot name="action" />
+    </v-toolbar>
+
+    <div class="pl-4 pr-4 pt-2 pb-2">
+      <slot>
+        No Content Defined
+      </slot>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  props: [
+    'title'
+  ]
 
 }
 </script>
